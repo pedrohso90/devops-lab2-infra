6908412ba3ae8614ab3bf4da394f9ee23f483af8
Vagrant User <vagrant@multicloud>
2021-08-14 10:42:50 -0300

    adicionando arquivos infra lab

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/vagrant/eng-archery-321513-7c2e742265ec.json")
+  #credentials = file("/home/vagrant/eng-archery-321513-7c2e742265ec.json")
 
   project = "eng-archery-321513"
   region  = "us-central1"
