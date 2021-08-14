ead1aeb27e620432f343776d077624b370e26715
Vagrant User <vagrant@multicloud>
2021-08-14 10:40:43 -0300

    adicionando arquivos infra lab

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.64.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/home/vagrant/eng-archery-321513-7c2e742265ec.json")
+
+  project = "eng-archery-321513"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
